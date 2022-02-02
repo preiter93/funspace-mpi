10326bde0219968063ea9d2b10b171b5e1866934
Philipp Markus Reiter <preiter@bomaxx16.lfpn.ds.mpg.de>
2022-02-02 17:47:30 +0100

    expose mpi type

--- src/mpi/mod.rs
+++ src/mpi/mod.rs
@@ -11,6 +11,7 @@ pub use mpi_decomp::mpi::environment::Universe;
 pub use mpi_decomp::mpi::initialize;
 pub use mpi_decomp::mpi::topology::Communicator;
 pub use mpi_decomp::mpi::traits::Equivalence;
+pub use mpi_decomp::mpi::collective::CommunicatorCollectives;
 pub use mpi_decomp::Decomp2d;
 pub use space2::Space2;
 pub use space_traits::BaseSpaceMpi;
